e117d1503f3ea2b98cfa95508138b59daa16b4ee
Taotu02feather <3436108@qq.com>
2026-03-27 16:50:47 +0800

    New_CIFAR10DVS_script

--- Code/Origin/script_CIFAR10DVS_BPTTTESS.sh
+++ Code/Origin/script_CIFAR10DVS_BPTTTESS.sh
@@ -1,7 +1,7 @@
 
 
-# TESS DVS Gesture
-CUDA_VISIBLE_DEVICES=0 python main.py --dataset DVSGesture --arch dvs_tessvgg_model --data-path ~/Datasets --save-path ./experiments/DVSGesture_TESS_ --trials 1 --epochs 200 --batch-size 16 --val-batch-size 16 --print-freq 20 --delay-ls 20 --factors-stdp 0.2 0.5 0 1 --pooling MAX --scheduler 100 --lr 0.001 --lr-conv 0.001 --experiment-name "DVSGesture" --training-mode tess --loss "CE" --wn --optimizer Adam
+# TESS DVSCIFAR10
+CUDA_VISIBLE_DEVICES=0 python main.py --dataset CIFAR10DVS --arch dvscifar10_tessvgg_model --data-path ~/Datasets --save-path ./experiments/CIFAR10DVS_VGG_TESS_ --trials 1 --epochs 200 --batch-size 64 --val-batch-size 64 --print-freq 20 --delay-ls 10 --factors-stdp 0.2 0.5 0 1 --pooling MAX --scheduler 100 --lr 0.001 --lr-conv 0.001 --experiment-name "DVSCIFAR10" --training-mode tess --loss "CE" --wn --optimizer Adam
 
-# BPTT DVS Gesture
-CUDA_VISIBLE_DEVICES=2 python main.py --dataset DVSGesture --arch dvs_vgg_bptt --data-path ~/Datasets --save-path ./experiments/DVSGesture_BPTT_ --trials 1 --epochs 200 --batch-size 16 --val-batch-size 16 --print-freq 20 --pooling MAX --scheduler 100 --lr 0.001 --lr-conv 0.001 --experiment-name "DVSGesture" --training-mode bptt --loss "CE" --wn --optimizer Adam
+# BPTT DVSCIFAR10
+CUDA_VISIBLE_DEVICES=0 python main.py --dataset CIFAR10DVS --arch dvscifar10_vgg_bptt --data-path ~/Datasets --save-path ./experiments/CIFAR10DVS_VGG_BPTT_ --trials 1 --epochs 200 --batch-size 64 --val-batch-size 64 --print-freq 20 --pooling MAX --scheduler 100 --lr 0.001 --lr-conv 0.001 --experiment-name "DVSCIFAR10" --training-mode bptt --loss "CE" --wn --optimizer Adam
